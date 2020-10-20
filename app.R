2d5f5dc5912cdb617be01a0b4b9c792789796dd8
willoutcault <57675974+willoutcault@users.noreply.github.com>
2020-10-19 22:59:26 -0400

    Update app.R

--- app.R
+++ app.R
@@ -8,6 +8,7 @@ require(DT)
 require(shinycssloaders)
 
 
+
 header <- dashboardHeader(
     title = "NYS COVID-19 TRACKER"
 )
@@ -15,9 +16,9 @@ header <- dashboardHeader(
 body <- dashboardBody(
     includeCSS("https://raw.githubusercontent.com/willoutcault/608_final/master/styles.css"),
     fluidRow(
-        column(width = 8,
+        column(width = 12,
                box(width = NULL, solidHeader = TRUE,
-                   leafletOutput("map", height = 400, width = 500) %>% withSpinner(color="#3c8dbc")
+                   leafletOutput("map", height = 800, width = 1000) %>% withSpinner(color="#3c8dbc")
                ),
                box(width = NULL,
                    DT::dataTableOutput("jobPositionsTable")
@@ -31,8 +32,8 @@ body <- dashboardBody(
                                   min    = "2020-03-01",
                                   max    = Sys.Date()),
                    actionButton("go", "Search")
-              )
-         )
+               )
+        )
     )
 )
 
@@ -48,47 +49,106 @@ server <- function(input,output){
     
     
     covid.data <- read.csv("https://health.data.ny.gov/resource/xdss-u53e.csv?$limit=100000", sep=",", header = T)
+    
     county.data <- rgdal::readOGR("https://raw.githubusercontent.com/willoutcault/covid/main/cugir-007865-geojson.json")
+    
     covid.data$test_date <- as.Date(covid.data$test_date)
 
-    county.data@data <- sp::merge(county.data@data, covid.data, by.x="name", by.y="county")
-View(county.data@data)    
     
     
     output$map <- renderLeaflet({
         
-        county.data.filtered <- county.data
-        county.data.filtered@data <- county.data.filtered@data %>%
+        covid.data.filtered <- covid.data %>%
             filter(test_date >= input$daterange[1] & test_date <= input$daterange[2]) %>% 
-            group_by(name) %>%
+            group_by(county) %>%
             summarize("new_positives" = sum(new_positives),
                       "cumulative_number_of_positives" = sum(cumulative_number_of_positives),
                       "total_number_of_tests" = sum(total_number_of_tests),
                       "cumulative_number_of_tests" = sum(cumulative_number_of_tests))
         
-        popup <- paste0("<strong>Name: </strong>",county.data.filtered@data$name,"<br>",
-                        "<strong>Cases: </strong>", county.data.filtered@data$new_positives)
+        date.range <- input$daterange[2] - input$daterange[1]
         
-        leaflet() %>% 
-            addTiles() %>% 
-            addPolygons(data=county.data, weight = 2, fillColor = "yellow", popup=popup)
+        covid.data.filtered.old <- covid.data %>%
+            filter(test_date >= (input$daterange[1]-date.range) & test_date <= (input$daterange[2]-date.range)) %>% 
+            group_by(county) %>%
+            summarize("new_positives" = sum(new_positives),
+                      "cumulative_number_of_positives" = sum(cumulative_number_of_positives),
+                      "total_number_of_tests" = sum(total_number_of_tests),
+                      "cumulative_number_of_tests" = sum(cumulative_number_of_tests))
+        
+        covid.data.filtered$pct_change <- round((covid.data.filtered$new_positives/covid.data.filtered.old$new_positives)
+                                                - 1, 2)
+            
+        
+        merged.spdf <- sp::merge(county.data, covid.data.filtered, by.x="name", by.y="county")
+        
+        popup <- paste0("<strong>Name: </strong>",merged.spdf@data$name,"<br>",
+                        "<strong>Cases: </strong>", merged.spdf@data$new_positives,"<br>",
+                        "<strong>Percent Change: </strong>", merged.spdf@data$pct_change)
+        
+        pal <- colorNumeric(
+            palette = "Blues",
+            domain = merged.spdf@data$new_positives)
+        
+        leaflet(merged.spdf) %>% 
+            setView(-76, 42.8, 7.4) %>%
+            addPolygons(weight = 2, popup=popup, stroke = FALSE, smoothFactor = 0.2,
+                        fillOpacity = 1, color = ~pal(new_positives))
+            
         
     })
+    
+    
+    
+    
     
     output$jobPositionsTable <- renderDataTable({
         
-        county.data.filtered <- county.data
-        county.data.filtered@data <- county.data.filtered@data %>%
+        covid.data.filtered <- covid.data %>%
             filter(test_date >= input$daterange[1] & test_date <= input$daterange[2]) %>% 
-            group_by(name) %>%
+            group_by(county) %>%
             summarize("new_positives" = sum(new_positives),
                       "cumulative_number_of_positives" = sum(cumulative_number_of_positives),
                       "total_number_of_tests" = sum(total_number_of_tests),
                       "cumulative_number_of_tests" = sum(cumulative_number_of_tests))
-        datatable(county.data.filtered@data, options = list(paging = FALSE, scrollY = "225px"), rownames = FALSE)
+        
+        merged.spdf <- sp::merge(county.data, covid.data.filtered, by.x="name", by.y="county")
+        
+        merged.spdf.ss <- select(merged.spdf@data, name, total_number_of_tests, cumulative_number_of_tests,
+                                 new_positives, cumulative_number_of_positives)
+        
+        merged.spdf.ss <- unique(covid.data.filtered)
+        
+        datatable(merged.spdf.ss, options = list(paging = FALSE, scrollY = "225px"), rownames = FALSE)
+        
     })
     
     
+    
+    
+    output$jobPositionsTable <- renderDataTable({
+        
+
+       # datatable(merged.spdf.ss, options = list(paging = FALSE, scrollY = "225px"), rownames = FALSE)
+        
+    })
+    
+    
+    output$plot<- renderPlot({
+        
+        ggplot(count_location(jobdetails()), aes(x=reorder(Location, n),y=(n/sum(n)), label="count")) +
+            geom_bar(stat='identity', width=.5, fill = "lightblue", color = "darkblue")  +
+            scale_fill_manual(name="Cities Hiring") + 
+            coord_flip()+
+            labs(title="Top 10 Cities", 
+                 subtitle="Based Off Total Open Positions") +
+            theme(axis.title.x = element_blank(),
+                  axis.title.y = element_blank(),
+                  text = element_text(size=13)) +
+            scale_y_continuous(labels = scales::percent) +
+            scale_x_discrete(position = "left")
+        
+    }, height = 188)
     
     
 }
