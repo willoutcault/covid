b14d5f720493b41bf2e3c4880cea28fee1a89e93
willoutcault <57675974+willoutcault@users.noreply.github.com>
2020-10-20 13:51:57 -0400

    Update app.R

--- app.R
+++ app.R
@@ -24,7 +24,7 @@ body <- dashboardBody(
                    DT::dataTableOutput("jobPositionsTable")
                )
         ),
-        column(width = 3,
+        column(width = 4,
                box(width = NULL, status = "warning",
                    dateRangeInput("daterange", "Date range:",
                                   start  = Sys.Date()-7,
@@ -37,10 +37,10 @@ body <- dashboardBody(
         column(width = 4,
                box(width = NULL, height = 650, status = "warning",
                    plotOutput("plot1",),
-                )
-            )
+               )
         )
     )
+)
 
 
 ui <- dashboardPage(
@@ -58,7 +58,7 @@ server <- function(input,output){
     county.data <- rgdal::readOGR("https://raw.githubusercontent.com/willoutcault/covid/main/cugir-007865-geojson.json")
     
     covid.data$test_date <- as.Date(covid.data$test_date)
-
+    
     aggregate <- function(daterange1, daterange2, covid.data){
         covid.data %>%
             filter(test_date >= daterange1 & test_date <= daterange2) %>% 
@@ -81,25 +81,28 @@ server <- function(input,output){
         covid.data.filtered.old <- aggregate(input$daterange[1]-date.range, input$daterange[2]-date.range, covid.data)
         
         covid.data.filtered$pct_change <- round(((covid.data.filtered$new_positives/
-                                                  covid.data.filtered.old$new_positives) - 1)*100, 2)
-            
-        covid.data.filtered$pct_change <- paste(covid.data.filtered$pct_change, "%", sep="")
+                                                      covid.data.filtered.old$new_positives) - 1)*100, 2)
+        
+        covid.data.filtered$old_positives <- covid.data.filtered.old$new_positives
         
         merged.spdf <- sp::merge(county.data, covid.data.filtered, by.x="name", by.y="county")
         
+        
         popup <- paste0("<strong>Name: </strong>",merged.spdf@data$name,"<br>",
                         "<strong>Cases: </strong>", merged.spdf@data$new_positives,"<br>",
-                        "<strong>Percent Change: </strong>", merged.spdf@data$pct_change)
+                        "<strong>Old Cases: </strong>", merged.spdf@data$old_positives,"<br>",
+                        "<strong>Percent Change: </strong>", merged.spdf@data$pct_change,"%")
         
         pal <- colorNumeric(
-            palette = "Blues",
+            palette = "Oranges",
             domain = merged.spdf@data$new_positives)
         
         leaflet(merged.spdf) %>% 
+            addProviderTiles(providers$CartoDB.DarkMatter) %>% 
             setView(-76, 42.8, 7.4) %>%
             addPolygons(weight = 2, popup=popup, stroke = FALSE, smoothFactor = 0.2,
                         fillOpacity = 1, color = ~pal(new_positives))
-            
+        
         
     })
     
@@ -119,41 +122,27 @@ server <- function(input,output){
         covid.data.filtered$pct_change <- round(((covid.data.filtered$new_positives/
                                                       covid.data.filtered.old$new_positives) - 1)*100, 2)
         
-        covid.data.filtered$pct_change_type <- ifelse(covid.data.filtered$pct_change < 0, "below", "above")
+        covid.data.filtered$pct_change_type <- ifelse(covid.data.filtered$pct_change < 0, "decrease", "increase")
+    
         
-        ggplot(covid.data.filtered, aes(x=county, y=pct_change, label=pct_change)) + 
+        ggplot(covid.data.filtered, aes(x=reorder(county, pct_change), y=pct_change, label=pct_change)) + 
             geom_bar(stat='identity', aes(fill=pct_change_type))  +
             scale_fill_manual(name="Mileage", 
-                              labels = c("Above Average", "Below Average"), 
-                              values = c("below"="#f8766d", "above"="#00ba38")) + 
-            labs(subtitle="Normalised mileage from 'mtcars'", 
-                 title= "Diverging Bars") + 
+                              labels = c("Increase", "Decrease"), 
+                              values = c("increase"="#f8766d", "decrease"="#00ba38")) + 
+            labs(title= "Percent Change in COVID-19 Cases") + 
             coord_flip()
         
         
     }, height = 650)
     
     
-    output$jobPositionsTable <- renderDataTable({
+    output$plot2<- renderPlot({
         
-        covid.data.filtered <- covid.data %>%
-            filter(test_date >= input$daterange[1] & test_date <= input$daterange[2]) %>% 
-            group_by(county) %>%
-            summarize("new_positives" = sum(new_positives),
-                      "cumulative_number_of_positives" = sum(cumulative_number_of_positives),
-                      "total_number_of_tests" = sum(total_number_of_tests),
-                      "cumulative_number_of_tests" = sum(cumulative_number_of_tests))
         
-        merged.spdf <- sp::merge(county.data, covid.data.filtered, by.x="name", by.y="county")
         
-        merged.spdf.ss <- select(merged.spdf@data, name, total_number_of_tests, cumulative_number_of_tests,
-                                 new_positives, cumulative_number_of_positives)
         
-        merged.spdf.ss <- unique(covid.data.filtered)
-        
-        datatable(merged.spdf.ss, options = list(paging = FALSE, scrollY = "225px"), rownames = FALSE)
-        
-    })
+    }, height = 650)
     
     
     
